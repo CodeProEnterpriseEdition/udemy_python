12444d446085f6cca93d6cbcc2d8125eac3ba29a
Pentti <pentti_korpela@hotmail.com>
2022-07-14 18:36:13 +0300

    docstring_functions

---
+++ docstring_functions.py
@@ -0,0 +1,11 @@
+
+from random import random
+
+def say_hello():
+    """A simple function that returns the string hello"""
+    return "Hello!"
+
+print(say_hello.__doc__)
+print(random.__doc__)
+
+
