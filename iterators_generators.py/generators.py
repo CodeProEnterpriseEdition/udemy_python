d62bae0cd2490d99f68d41c1cf9deaa84ce5e933
Pentti <pentti_korpela@hotmail.com>
2022-07-18 15:34:45 +0300

    iterators & generators

--- iterators_generators.py/generators.py
+++ iterators_generators.py/generators.py
@@ -5,22 +5,36 @@
 #         yield next_num
 #         next_num += number
     
-def get_multiples(num=1, count=10):
-    next_num = num
-    while count > 0:
-        yield next_num
-        count -= 1
-        next_num += num
+# def get_multiples(num=1, count=10):
+#     next_num = num
+#     while count > 0:
+#         yield next_num
+#         count -= 1
+#         next_num += num
 
 
-gene = (num for num in range(1,10))
-print(gene)
-print(next(gene))
-print(next(gene))
-print(next(gene))
+# gene = (num for num in range(1,10))
+# print(gene)
+# print(next(gene))
+# print(next(gene))
+# print(next(gene))
 
-#  vs LIST
+# #  vs LIST
 
-lst = [num for num in range(1,10)]
+# lst = [num for num in range(1,10)]
 
-print(lst)
+# print(lst)
+
+import time
+
+gen_start_time = time.time()
+print(sum(n for n in range(100000000)))
+gen_time = time.time() - gen_start_time
+
+list_start_time = time.time()
+print(sum([n for n in range(100000000)]))
+list_time = time.time() - list_start_time
+
+
+print(f"generator time {gen_time}")
+print(f"list time {list_time}")
